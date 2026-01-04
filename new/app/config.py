c94a0834af51e47a55e63fad18deb67c49a3155b
lvle925 <2627637855@qq.com>
2026-01-04 11:17:47 +0800

    Delete new directory

--- new/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# Astro API 配置
-ASTRO_API_URL = os.getenv("ASTRO_API_URL")
--- new/app/ziwei_service.py
+++
@@ -1,8 +0,0 @@
-# app/ziwei_service.py (简略版，确保包含 process_yearly_fortune)
-from app.processing import get_yearly_scores_only, get_monthly_scores
-from app.utils import convert_time_to_time_index
-# ... (其他 import 和常量定义) ...
-
-async def process_yearly_fortune(request):
-    # ... (原有 api.py 中的 calculate 逻辑) ...
-    pass
